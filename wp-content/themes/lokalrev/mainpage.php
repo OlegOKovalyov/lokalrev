00b639adae69f87397100380a52fcb8d2daeb6b5
Oleg Kovalyov <koa2003@ukr.net>
2019-10-29 19:17:53 +0200

    DT-70 Homepage is in progress. Pink Section is remained.

--- wp-content/themes/lokalrev/mainpage.php
+++ wp-content/themes/lokalrev/mainpage.php
@@ -12,6 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+
             <!-- Hero Section -->
             <?php $hero = get_field('hero_section'); ?>
             <?php if( $hero ): ?>
@@ -35,22 +36,94 @@ get_header();
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
-                            <blockquote class="blockquote">
-                                <?php the_field('quote_text'); ?>
-                                <div class="blockquote-footer"><?php the_field('quote_author'); ?></div>
-                            </blockquote>
+                            <div class="row">
+                                <blockquote class="blockquote">
+                                    <?php the_field('quote_text'); ?>
+                                    <div class="blockquote-footer"><?php the_field('quote_author'); ?></div>
+                                </blockquote>
+                            </div>
                         </div>
                         <div class="col-lg-7">
-                            <?php echo do_shortcode('[contact-form-7 id="54" title="Form On Mainpage"]'); ?>
+                            <div class="row">
+                                <?php echo do_shortcode('[contact-form-7 id="54" title="Form On Mainpage"]'); ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Red Block Section -->
+            <?php $red_section = get_field('red_section'); ?>
+            <?php if( $red_section ): ?>
+            <div class="red-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-10">
+                            <h2><?php echo $red_section['title']; ?></h2>
+                        </div>
+                        <div class="row">
+                            <div class="col col-icon">
+                                <img class="i-image" src="<?php echo $red_section['icon1'] ?>" alt="">
+                                <h4><?php echo $red_section['title_1'] ?></h4>
+                                <p><?php echo $red_section['text_1'] ?></p>
+                            </div>
+                            <div class="col col-icon">
+                                <img class="i-image" src="<?php echo $red_section['icon2'] ?>" alt="">
+                                <h4><?php echo $red_section['title_2'] ?></h4>
+                                <p><?php echo $red_section['text_2'] ?></p>
+                            </div>
+                            <div class="col col-icon">
+                                <img class="i-image" src="<?php echo $red_section['icon3'] ?>" alt="">
+                                <h4><?php echo $red_section['title_3'] ?></h4>
+                                <p><?php echo $red_section['text_3'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
+
+            <!-- Slider Section -->
+            <div class="slider-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3><?php the_field('subtitle_after_red_block'); ?></h3>
+                            <?php the_field('text_after_red_block'); ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <?php echo do_shortcode('[slide-anything id=\'88\']') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pink Block Section -->
+            <?php $pink_section = get_field('pink_section'); ?>
+            <?php if( $pink_section ): ?>
+            <div class="pink-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h2><?php echo $pink_section['title']; ?></h2>
+                            <?php echo $pink_section['text1']; ?>
+                        </div>
+                        <div class="col-lg-6">
+                            <span></span><span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
+
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			//get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
